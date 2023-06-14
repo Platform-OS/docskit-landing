a9221f283f54ee1e28d06108fca3674fa2284db1
Peter Almasi <Peter_Almasi@outlook.com>
2023-06-14 15:04:39 +0200

    init

---
+++ gatsby-config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+module.exports = {
+  siteMetadata: {
+    title: 'platformOS DocsKit',
+    description: 'platformOS DocsKit',
+    author: 'platformOS'
+  },
+    plugins: [
+      {
+        resolve: '@platformos/gatsby-theme-platformos-docskit',
+        options: {
+          useLegacyUrls: true
+        }
+      },
+      {
+        resolve: 'gatsby-plugin-manifest',
+        options: {
+          name: 'platformOS DocsKit',
+          short_name: 'DocsKit',
+          start_url: '/',
+          background_color: '#663399',
+          display: 'minimal-ui',
+          icon: path.join(__dirname, 'src/images/favicon.png'), // This path is relative to the root of the site.
+        },
+      },
+    ],
+  }
