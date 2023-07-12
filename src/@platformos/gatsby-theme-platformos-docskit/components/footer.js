a8785842cfc8e77a3671d10aeb6c318c33d5dc81
Peter Almasi <Peter_Almasi@outlook.com>
2023-07-12 13:04:02 +0200

    license

--- src/@platformos/gatsby-theme-platformos-docskit/components/footer.js
+++ src/@platformos/gatsby-theme-platformos-docskit/components/footer.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Logo from '@platformos/gatsby-theme-platformos-docskit/src/components/logo';
 import './footer.css';
+import { Link } from 'gatsby';
 
 const Footer = ({ children }) => (
   <footer className="footer bg-base p-6 text-sm">
@@ -9,6 +10,7 @@ const Footer = ({ children }) => (
       <span>platformOS DocsKit &copy; {new Date().getFullYear()}</span>
       <span>powered by <a href="https://platformos.com" target="_blank" rel="noopener noreferrer"><u>platformOS</u></a></span>
       <span>built with <a href="https://www.gatsbyjs.org" target="_blank" rel="noopener noreferrer"><u>Gatsby</u></a></span>
+      <Link to="/license/"><u>License</u></Link>
     </div>
   </footer>
 );
