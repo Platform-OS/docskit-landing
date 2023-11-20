a031bae73ff66f432655305a34191e9f41141828
Peter Almasi <Peter_Almasi@outlook.com>
2023-11-20 18:29:59 +0100

    courses wip 2

---
+++ modules/courses/public/assets/courses.js
@@ -0,0 +1,3 @@
+document.querySelector('[data-navigation-toggle]').addEventListener('click', () => {
+  document.querySelector('[data-nav-container]').classList.toggle('courses__sidebar--open');
+});
