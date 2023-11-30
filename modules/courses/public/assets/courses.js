240ee72a67fb015e0d9f59b70d0592a249c4281e
Peter Almasi <Peter_Almasi@outlook.com>
2023-11-30 12:23:22 +0100

    Courses module: Add esbuild

--- modules/courses/public/assets/courses.js
+++ modules/courses/public/assets/courses.js
@@ -1,12 +1,1 @@
-// responsive navigation
-document.querySelector('[data-navigation-toggle]').addEventListener('click', () => {
-  document.querySelector('[data-nav-container]').classList.toggle('courses__sidebar--open');
-});
-
-// sidebar accordion
-document.querySelectorAll('[data-nav-container] .module-list__item').forEach(listItem => {
-  const trigger = listItem.querySelector('.module-list__item-title');
-  trigger.addEventListener('click', () => {
-    listItem.classList.toggle('module-list__item--active');
-  });
-});
+(()=>{document.querySelector("[data-navigation-toggle]").addEventListener("click",()=>{document.querySelector("[data-nav-container]").classList.toggle("courses__sidebar--open")});document.querySelectorAll("[data-nav-container] .module-list__item").forEach(e=>{e.querySelector(".module-list__item-title").addEventListener("click",()=>{e.classList.toggle("module-list__item--active")})});})();
---
+++ modules/courses/public/src/courses.js
@@ -0,0 +1,14 @@
+import './courses.css';
+
+// responsive navigation
+document.querySelector('[data-navigation-toggle]').addEventListener('click', () => {
+  document.querySelector('[data-nav-container]').classList.toggle('courses__sidebar--open');
+});
+
+// sidebar accordion
+document.querySelectorAll('[data-nav-container] .module-list__item').forEach(listItem => {
+  const trigger = listItem.querySelector('.module-list__item-title');
+  trigger.addEventListener('click', () => {
+    listItem.classList.toggle('module-list__item--active');
+  });
+});
