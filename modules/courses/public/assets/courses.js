22058d7271d17430283262c7f9552fd9a24b6b5d
Peter Almasi <Peter_Almasi@outlook.com>
2023-12-01 08:50:32 +0100

    Courses module: validation; style adjustments

--- modules/courses/public/assets/courses.js
+++ modules/courses/public/assets/courses.js
@@ -1,13 +1,1 @@
-(() => {
-  // src/courses.js
-  document.querySelector("[data-navigation-toggle]").addEventListener("click", () => {
-    document.querySelector("[data-nav-container]").classList.toggle("courses__sidebar--open");
-  });
-  document.querySelectorAll("[data-nav-container] .module-list__item").forEach((listItem) => {
-    const trigger = listItem.querySelector(".module-list__item-title");
-    trigger.addEventListener("click", () => {
-      listItem.classList.toggle("module-list__item--active");
-    });
-  });
-})();
-//# sourceMappingURL=courses.js.map
+(()=>{document.querySelector("[data-navigation-toggle]").addEventListener("click",()=>{document.querySelector("[data-nav-container]").classList.toggle("courses__sidebar--open")});document.querySelectorAll("[data-nav-container] .module-list__item").forEach(e=>{e.querySelector(".module-list__item-title").addEventListener("click",()=>{e.classList.toggle("module-list__item--active")})});})();
