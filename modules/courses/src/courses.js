7f5b3f0132079974b2c7099c9e4b1079c5295df2
Peter Almasi <Peter_Almasi@outlook.com>
2023-12-07 12:25:19 +0100

    Courses module: demo updates

--- modules/courses/src/courses.js
+++ modules/courses/src/courses.js
@@ -1,14 +1,22 @@
 import './courses.css';
 
 // responsive navigation
-document.querySelector('[data-navigation-toggle]').addEventListener('click', () => {
-  document.querySelector('[data-nav-container]').classList.toggle('courses__sidebar--open');
-});
+const navigationToggle = document.querySelector('[data-navigation-toggle]');
+
+if (navigationToggle) {
+  document.querySelector('[data-navigation-toggle]').addEventListener('click', () => {
+    document.querySelector('[data-nav-container]').classList.toggle('courses__sidebar--open');
+  });
+}
 
 // sidebar accordion
-document.querySelectorAll('[data-nav-container] .module-list__item').forEach(listItem => {
-  const trigger = listItem.querySelector('.module-list__item-title');
-  trigger.addEventListener('click', () => {
-    listItem.classList.toggle('module-list__item--active');
+const navContainer = document.querySelector('[data-nav-container]');
+
+if (navContainer) {
+  navContainer.querySelectorAll('[data-nav-container] .module-list__item').forEach(listItem => {
+    const trigger = listItem.querySelector('.module-list__item-title');
+    trigger.addEventListener('click', () => {
+      listItem.classList.toggle('module-list__item--active');
+    });
   });
-});
+}
