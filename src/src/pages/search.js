a49f57b7f887c86da93908817770ab8e7c89c7a9
Tibor <bratoblog@gmail.com>
2024-04-22 13:57:14 +0200

    remove old search functionality

--- src/src/pages/search.js
+++ src/src/pages/search.js
@@ -1,47 +1,16 @@
 import * as React from 'react';
 import { Link, graphql } from 'gatsby';
 import ThemeProvider from '@platformos/gatsby-theme-platformos-docskit/src/theme/theme-provider';
-import queryString from 'query-string';
 import Layout from '@components/layout';
 import Seo from '@components/seo';
-import { useFlexSearch } from 'react-use-flexsearch';
 
-export default function SearchPage({ data, location, pageContext }) {
-  const [query, setQuery] = React.useState(null);
-  const { navigationTree, metaData } = pageContext;
-  const { localSearchPages: { index, store } } = data;
-  const results = useFlexSearch(query, index, store).filter((item) => !!item.slug);
 
-  React.useEffect(() => {
-    const { q: query } = queryString.parse(location.search);
-    setQuery(query || '');
-  }, [location.search]);
-
+export default function SearchPage() {
   return (
-    <Layout
-      location={location}
-      currentSlug="search"
-      sidebarEnabled
-      tocEnabled={false}
-      treeData={navigationTree}
-      metaData={metaData}
-    >
+    <Layout>
       <div>
         <h1 className="text-3xl font-bold">Search results</h1>
-        <h2 className="text-2xl font-bold pb-4">{query ? `for "${query}"` : ''}</h2>
-        {(query !== null) ? (<>
-          <div className="mb-8 text-supplementary">{results.length} results</div>
-          <ul className="">
-            {results.map((item) => (
-              <li key={item.slug} className="mb-4 py-4 border-b">
-                <Link to={`/${item.slug}`}>{item.title}</Link>
-                <div className="text-sm text-supplementary mb-4">{item.description}</div>
-              </li>
-            ))}
-          </ul>
-        </>) : (
-          <div className="mb-8 text-supplementary">Loading results...</div>
-        )}
+
       </div>
     </Layout>
   );
