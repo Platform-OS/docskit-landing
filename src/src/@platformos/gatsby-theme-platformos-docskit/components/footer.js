90c1184fc275a55dad4b97217a78b52b80660c5a
Marcellokah <almasimarcell@gmail.com>
2025-06-16 10:27:34 +0200

    Footer logo <a> tag

--- src/src/@platformos/gatsby-theme-platformos-docskit/components/footer.js
+++ src/src/@platformos/gatsby-theme-platformos-docskit/components/footer.js
@@ -6,7 +6,7 @@ import { Link } from 'gatsby';
 const Footer = ({ children }) => (
   <footer className="footer bg-base p-6 text-sm">
     <div className="footer__content container mx-auto">
-      <div className='footer__logo'><Logo /></div>
+      <a href='/' className='footer__logo'><Logo /></a>
       <span>platformOS DocsKit &copy; {new Date().getFullYear()}</span>
       <span>built with DocsKit</span>
       <span>powered by <a href="https://platformos.com" target="_blank" rel="noopener noreferrer"><u>platformOS</u></a></span>
